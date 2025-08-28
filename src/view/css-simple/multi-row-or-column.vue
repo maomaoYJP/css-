f14d7eb6d29c21490d701497c0337aa897d6147c
maomao <2081498716@qq.com>
2025-08-28 23:31:26 +0800

    跨行或跨列布局

---
+++ src/view/css-simple/multi-row-or-column.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full h-full flex">
+    <div
+      class="hover:flex-row group h-100 w-100 m-auto bg-blue-200 flex flex-col flex-wrap p-4 gap-2"
+    >
+      <div
+        class="transition-all group-hover:w-full group-hover:h-[calc(50%-4px)] h-full w-[calc(50%-4px)] border-2 border-gray-200 rounded-2xl"
+      ></div>
+      <div
+        class="transition-all h-[calc(50%-4px)] w-[calc(50%-4px)] border-2 border-gray-200 rounded-2xl"
+      ></div>
+      <div
+        class="transition-all h-[calc(50%-4px)] w-[calc(50%-4px)] border-2 border-gray-200 rounded-2xl"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
